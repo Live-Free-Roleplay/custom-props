dc3818cfa824e81aed8a1fbc127f403576ff37df
Clink123 <clink123gamer@gmail.com>
2024-05-26 22:09:56 -0400

    Add custom config

--- config.lua
+++ config.lua
@@ -3,6 +3,10 @@ Config = {}
 Config.Props = {
     [1] = {
         model = "bkr_prop_meth_table01a",
-        coords = vector4(1609.5, 3574.56, 38.78-0.9, 115.54),
+        coords = vector4(1609.0743, 3574.2556, 38.7752, 298.1664),
+    },
+    [2] = {
+        model = "v_ret_ml_tablea",
+        coords = vector4(-633.52, -1244.95, 16.86, 344.78),
     },
 }
